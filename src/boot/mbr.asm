a202afecca4a68cbe71fc25eed0757b5778fb147
Joel Souza <joelsantossouza08@gmail.com>
2026-03-04 22:31:58 +0000

    Implementing boot_panic

--- src/boot/mbr.asm
+++ src/boot/mbr.asm
@@ -5,9 +5,10 @@
 ; 			   the hard disk. It will load the stage 2 of boot into RAM.
 
 %include "drivers/disk/vdl/config.inc"
+%include "boot/config.inc"
+%include "boot/bios.inc"
 %include "cpu/gdt.inc"
 %include "cpu/cr0.inc"
-%include "boot/config.inc"
 
 jmp	short boot_start
 nop
@@ -52,23 +53,71 @@ gdt_descriptor:
 	dw	gdt.end - gdt - 1
 	dd	gdt
 
+[BITS 16]
+;;
+; boot_panic - Panic on boot phase
+;
+; DESCRIPTION
+;	Prints an error message on screen and enters an
+;	infinite loop, preventing further execution
+;
+; RETURN VALUE
+;	None
+;;
+BOOT_PANIC_STR:		db "Error: Could not boot system"
+BOOT_PANIC_STRLEN	equ $ - BOOT_PANIC_STR
+boot_panic:
+	mov	ah, BIOS_STR_WRITE
+	mov	al, 0x0
+	mov	bh, 0x0
+	mov	bl, BIOS_TEXT_RED
+	mov	cx, BOOT_PANIC_STRLEN
+	mov	dh, 0x0
+	mov	dl, 0x0
+	mov	bp, BOOT_PANIC_STR
+	int	BIOS_INT_VIDEO
+	jmp	$
+
 ;;
 ; boot_start - Boot code
 ;
 ; DESCRIPTION
+;	-> Save the booted drive number
+;	-> Init segment registers (16-bit mode)
 ;	-> Load boot stage 2 in RAM
 ;	-> Loads GDT
 ;	-> Enter in Protected Mode
-;	-> Init segment registers
+;	-> Init segment registers (32-bit mode)
 ;	-> Jump to boot stage 2 (Where will be loaded the kernel)
 ;
 ; RETURN VALUE
 ;	None
 ;;
+BOOT_DRIVE_NO:	db 0
 boot_start:
-[BITS 16]
+	mov	[BOOT_DRIVE_NO], dl	; Save boot drive number set by BIOS
+
+.init_16bit_mode:
+	mov	ax, 0x0
+	mov	ds, ax
+	mov	ss, ax
+	mov	es, ax
+	mov	fs, ax
+	mov gs, ax
+
 .load_stage2:
-; TODO: Write load of stage2
+	mov	ah, BIOS_DRIVE_READ
+	mov	al, BOOT_STAGE2_SECTORS
+	mov	ch, BOOT_STAGE2_CYLINDER
+	mov	cl, BOOT_STAGE2_SECTOR
+	mov	dh, BOOT_STAGE2_HEAD
+	mov	dl, BOOT_DRIVE_NO
+	mov	bx, BOOT_STAGE2_ADDR
+	int	BIOS_INT_DRIVE
+
+	jc	boot_panic
+	cmp	al, BOOT_STAGE2_SECTORS
+	jne	boot_panic
 
 .enter_32bit_mode:
 	cli
