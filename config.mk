30db2b3f31269db1838e99cb0137a2d380e64419
Joel Souza <joelsantossouza08@gmail.com>
2026-03-07 11:02:36 +0000

    Trying to implement the deps files to rebuild files only if the include files have changed

--- config.mk
+++ config.mk
@@ -15,16 +15,3 @@ AFLAGS_ELF		:= $(AFLAGS) -f elf32 -g -F dwarf
 INCLUDES_DIR	:= $(ROOT)/include
 
 INCLUDES		:= -I$(INCLUDES_DIR)
-HEADERS			:= $(addprefix $(INCLUDES_DIR)/, \
-					boot/config.inc \
-					cpu/cr0.inc \
-					cpu/gdt.inc \
-					drivers/disk/ata/ata.h \
-					drivers/disk/vdl/config.inc \
-					drivers/disk/vdl/vdl.h \
-					drivers/io/io.h \
-					fs/fat16/fat16.h \
-					fs/vfs/config.h \
-					fs/vfs/vfs.h \
-					kernel/config.h \
-)
