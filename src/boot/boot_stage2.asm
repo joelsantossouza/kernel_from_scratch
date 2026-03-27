9fee889e6242b6dde84d43709666fd3746a44b79
Joel Souza <joelsantossouza08@gmail.com>
2026-03-27 01:57:32 +0000

    Starting tests of fat_subdir_open()

--- src/boot/boot_stage2.asm
+++ src/boot/boot_stage2.asm
@@ -20,21 +20,42 @@ boot_stage2:
 	call	vfs_partition_init
 	add		esp, 12
 
-	; Read clusters
-	push	512
-	push	buffer
+	; Init subdir
+	push	32
+	push	subdir
 	push	offset
 	push	cluster
 	push	partition0
 	call	fat_cluster_read
 	add		esp, 20
+	mov		ax, word [subdir + 20]
+	shl		eax, 16
+	mov		ax, [subdir + 26]
+	mov		dword [subdir + 36], eax
+
+	; open file in subdir
+	push	file
+	push	file_next
+	push	file_name
+	push	subdir
+	call	fat_subdir_open
+	add		esp, 16
 
 	jmp	$
 
 section	.data
-buffer: times 512 db 0
-offset: dd 5
+offset:	dd 0
 cluster: dd 2
+
+file_next: dd 0
+file_name: db "kernel", 0
+
+file: times 32 db 0
+
+subdir:
+	times 32 db 0
+	dd	partition0
+	dd	0
 
 partition0: times 1024 db 0
 
