cded44bc78d4468433b2de11223d194334c7bdac
Joel Souza <joelsantossouza08@gmail.com>
2026-03-30 23:59:52 +0100

    The video_text_history_write() should cap the number of characters to write, by doing if (size > MAX) size = MAX

--- src/drivers/video/text/video_text_history.c
+++ src/drivers/video/text/video_text_history.c
@@ -14,16 +14,14 @@ uint16_t	g_video_text_history[VIDEO_TEXT_HISTORY_MAX];
 
 uint32_t	video_text_history_write(uint32_t offset, const char *text, uint32_t size, uint8_t attr)
 {
-	uint32_t	write_len;
 	uint32_t	remaining_until_limit;
 
 	if (offset >= VIDEO_TEXT_HISTORY_MAX)
 		offset %= VIDEO_TEXT_HISTORY_MAX;
 	if (size > VIDEO_TEXT_HISTORY_MAX)
 	{
-		write_len = size % VIDEO_TEXT_HISTORY_MAX;
-		text += size - write_len;
-		size = write_len;
+		text += VIDEO_TEXT_HISTORY_MAX - size;
+		size = VIDEO_TEXT_HISTORY_MAX;
 	}
 	remaining_until_limit = VIDEO_TEXT_HISTORY_MAX - offset;
 	if (size > remaining_until_limit)
