a5e1d772c70326977a30a5eda660abe4fff0dd19
Joel Souza <joelsantossouza08@gmail.com>
2026-03-31 02:16:45 +0100

    Starting implementation of video_text_vga_write()

--- src/drivers/video/text/video_text_history.c
+++ src/drivers/video/text/video_text_history.c
@@ -10,51 +10,54 @@
 #include "drivers/video/text/config.h"
 #include "string/string.h"
 
-uint16_t	g_video_text_history[VIDEO_TEXT_HISTORY_MAX];
+static uint16_t	g_video_text_history[VIDEO_TEXT_HISTORY_MAX];
 
-uint32_t	video_text_history_write(uint32_t offset, const char *text, uint32_t size, uint8_t attr)
+uint32_t	video_text_history_write(uint32_t offset, const char *text, uint32_t count, uint8_t attr)
 {
-	uint32_t	remaining_until_limit;
+	uint32_t	space_until_wrap_up;
 
 	if (offset >= VIDEO_TEXT_HISTORY_MAX)
 		offset %= VIDEO_TEXT_HISTORY_MAX;
-	if (size > VIDEO_TEXT_HISTORY_MAX)
+	if (count > VIDEO_TEXT_HISTORY_MAX)
 	{
-		text += VIDEO_TEXT_HISTORY_MAX - size;
-		size = VIDEO_TEXT_HISTORY_MAX;
+		text += count - VIDEO_TEXT_HISTORY_MAX;
+		count = VIDEO_TEXT_HISTORY_MAX;
 	}
-	remaining_until_limit = VIDEO_TEXT_HISTORY_MAX - offset;
-	if (size > remaining_until_limit)
+	space_until_wrap_up = VIDEO_TEXT_HISTORY_MAX - offset;
+	if (count > space_until_wrap_up)
 	{
-		memicpy8(&g_video_text_history[offset], text, remaining_until_limit, attr);
-		offset = size - remaining_until_limit;
-		memicpy8(g_video_text_history, &text[remaining_until_limit], offset, attr);
+		memicpy8(&g_video_text_history[offset], text, space_until_wrap_up, attr);
+		offset = count - space_until_wrap_up;
+		memicpy8(g_video_text_history, &text[space_until_wrap_up], offset, attr);
 	}
 	else
 	{
-		memicpy8(&g_video_text_history[offset], text, size, attr);
-		offset = size;
+		memicpy8(&g_video_text_history[offset], text, count, attr);
+		offset = count;
 	}
 	return (offset);
 }
 
-uint32_t	video_text_history_read(uint32_t offset, uint16_t *buf, uint32_t size)
+uint32_t	video_text_history_read(uint32_t offset, uint16_t *buf, uint32_t count)
 {
+	uint32_t	space_until_wrap_down;
+
 	if (offset >= VIDEO_TEXT_HISTORY_MAX)
 		offset %= VIDEO_TEXT_HISTORY_MAX;
-	if (size > VIDEO_TEXT_HISTORY_MAX)
-		size = VIDEO_TEXT_HISTORY_MAX;
-	if (size > offset)
+	if (count > VIDEO_TEXT_HISTORY_MAX)
+		count = VIDEO_TEXT_HISTORY_MAX;
+	space_until_wrap_down = offset + 1;
+	if (count > space_until_wrap_down)
 	{
-		size -= offset;
-		memcpy(&buf[size], g_video_text_history, offset);
-		offset = VIDEO_TEXT_HISTORY_MAX - size;
-		memcpy(buf, &g_video_text_history[offset], size);
+		count -= space_until_wrap_down;
+		memcpy(&buf[count], g_video_text_history, space_until_wrap_down * sizeof(uint16_t));
+		offset = VIDEO_TEXT_HISTORY_MAX - count;
+		memcpy(buf, &g_video_text_history[offset], count * sizeof(uint16_t));
 	}
 	else
 	{
-		offset -= size;
-		memcpy(buf, &g_video_text_history[offset], size);
+		offset = space_until_wrap_down - count;
+		memcpy(buf, &g_video_text_history[offset], count * sizeof(uint16_t));
 	}
 	return (offset);
 }
--- src/drivers/video/text/video_text_vga.c
+++ src/drivers/video/text/video_text_vga.c
@@ -6,3 +6,7 @@
  * 				and manage configurations of VGA text mode
  */
 
+void	video_text_vga_write(const char *text, uint32_t count, enum e_vga_text_color color)
+{
+	
+}
