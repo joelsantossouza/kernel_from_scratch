b5ece236c8e5c472e60540597d1f6e4336d60709
Joel Souza <joelsantossouza08@gmail.com>
2026-03-31 23:04:46 +0100

    Insert a history size tracker that limits the history read to its current size, avoiding situations where its read uninitialized data

--- src/drivers/video/text/video_text_history.c
+++ src/drivers/video/text/video_text_history.c
@@ -9,16 +9,16 @@
 #include <stdint.h>
 #include "drivers/video/text/config.h"
 #include "string/string.h"
+#include "math/math.h"
 
 static uint16_t	g_video_text_history[VIDEO_TEXT_HISTORY_MAX];
+static uint32_t	g_video_text_history_size = 0;
 static uint32_t	g_video_text_history_offset = 0;
 
 uint32_t	video_text_history_write(const char *text, uint32_t count, uint8_t attr)
 {
 	uint32_t	space_until_wrap_up;
 
-	if (g_video_text_history_offset >= VIDEO_TEXT_HISTORY_MAX)
-		g_video_text_history_offset %= VIDEO_TEXT_HISTORY_MAX;
 	if (count > VIDEO_TEXT_HISTORY_MAX)
 	{
 		text += count - VIDEO_TEXT_HISTORY_MAX;
@@ -36,35 +36,33 @@ uint32_t	video_text_history_write(const char *text, uint32_t count, uint8_t attr
 		memicpy8(&g_video_text_history[g_video_text_history_offset], text, count, attr);
 		g_video_text_history_offset += count;
 	}
+	if (g_video_text_history_size < VIDEO_TEXT_HISTORY_MAX)
+		g_video_text_history_size = MIN(g_video_text_history_size + count, VIDEO_TEXT_HISTORY_MAX);
 	return (count);
 }
 
 uint32_t	video_text_history_read(uint32_t rewind, uint16_t *buf, uint32_t count)
 {
 	uint32_t	space_until_wrap_down;
-	uint32_t	max_to_read;
-	uint32_t	entries_to_read;
+	uint32_t	second_chunk_size;
 	uint32_t	offset;
 
-	if (rewind >= VIDEO_TEXT_HISTORY_MAX)
+	if (rewind >= g_video_text_history_size)
 		return (0);
-	max_to_read = VIDEO_TEXT_HISTORY_MAX - rewind;
-	offset = (g_video_text_history_offset + VIDEO_TEXT_HISTORY_MAX - rewind) % VIDEO_TEXT_HISTORY_MAX;
-	if (count > max_to_read)
-		count = max_to_read;
-	entries_to_read = count;
+	offset = (g_video_text_history_offset + VIDEO_TEXT_HISTORY_MAX - rewind - 1) % VIDEO_TEXT_HISTORY_MAX;
+	count = MIN(count, g_video_text_history_size - rewind);
 	space_until_wrap_down = offset + 1;
 	if (count > space_until_wrap_down)
 	{
-		count -= space_until_wrap_down;
-		memcpy(&buf[count], g_video_text_history, space_until_wrap_down * sizeof(uint16_t));
-		offset = VIDEO_TEXT_HISTORY_MAX - count;
-		memcpy(buf, &g_video_text_history[offset], count * sizeof(uint16_t));
+		second_chunk_size = count - space_until_wrap_down;
+		memcpy(&buf[second_chunk_size], g_video_text_history, space_until_wrap_down * sizeof(uint16_t));
+		offset = VIDEO_TEXT_HISTORY_MAX - second_chunk_size;
+		memcpy(buf, &g_video_text_history[offset], second_chunk_size * sizeof(uint16_t));
 	}
 	else
 	{
 		offset = space_until_wrap_down - count;
 		memcpy(buf, &g_video_text_history[offset], count * sizeof(uint16_t));
 	}
-	return (entries_to_read);
+	return (count);
 }
--- src/drivers/video/text/video_text_vga.c
+++ src/drivers/video/text/video_text_vga.c
@@ -15,7 +15,7 @@ static t_video_text_config	g_video_text_vga_config = {
 	.height = VIDEO_TEXT_DEFAULT_HEIGHT,
 	.screensize = VIDEO_TEXT_DEFAULT_WIDTH * VIDEO_TEXT_DEFAULT_HEIGHT
 };
-static uint32_t				g_video_text_scroll = 0;
+static uint32_t				g_video_text_vga_scroll = 0;
 
 uint32_t	video_text_vga_write(const char *text, uint32_t count, enum e_vga_text_color color)
 {
@@ -24,7 +24,7 @@ uint32_t	video_text_vga_write(const char *text, uint32_t count, enum e_vga_text_
 
 	history_entries_written = video_text_history_write(text, count, color);
 	vga_entries_to_display = MIN(history_entries_written, g_video_text_vga_config.screensize);
-	g_video_text_scroll = 0;
+	g_video_text_vga_scroll = 0;
 	video_text_history_read(0, VIDEO_TEXT_VGA_ADDR, vga_entries_to_display);
 	return (vga_entries_to_display);
 }
